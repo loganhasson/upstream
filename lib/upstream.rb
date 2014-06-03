31eacfefc1227abab5f4c09bec9e2652094ea0b8
Logan Hasson <logan.hasson@gmail.com>
2014-06-03 11:22:58 -0400

    Maybe working?

--- lib/upstream.rb
+++ lib/upstream.rb
@@ -1,5 +1,101 @@
 require "upstream/version"
 
 module Upstream
-  # Your code goes here...
+
+  class Tracker
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def self.run(path)
+      new(path).setup
+    end
+
+    def setup
+      wheneverizer = Wheneverizer.new(path)
+      wheneverizer.run
+    end
+  end
+
+  class PathFinder
+    attr_reader :raw_path
+
+    def initialize(arg)
+      @raw_path = arg
+    end
+
+    def self.get_path(arg)
+      new(arg).parse
+    end
+
+    def parse
+      if raw_path == '.'
+        `pwd`.chomp
+      else
+        raw_path
+      end
+    end
+  end
+
+  class PathChecker
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def self.has_upstream?(path)
+      new(path).check
+    end
+
+    def check
+      `cd #{path} && git remote -v`.match(/upstream/) ? true : false
+    end
+  end
+
+  class Wheneverizer
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def run
+      setup
+      write_schedule
+      wheneverize
+      clean_up
+    end
+
+    def setup
+      `cd #{path} && wheneverize .`
+    end
+
+    def write_schedule
+      File.open('config/schedule.rb', 'w+') do |f|
+        f.write <<-COMMAND.stripheredoc.chomp
+          every 1.hour do
+            command "cd #{path} && git fetch upstream master"
+          end
+        COMMAND
+      end
+    end
+
+    def wheneverize
+      `whenever --update-crontab`
+    end
+
+    def clean_up
+      file_count = `ls config -a | wc -l`.strip
+
+      if file_count > 3
+        `rm config/schedule.rb`
+      else
+        `rm -rf config`
+      end
+    end
+  end
+
 end
--- upstream.gemspec
+++ upstream.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Upstream::VERSION
   spec.authors       = ["Logan Hasson"]
   spec.email         = ["logan.hasson@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Upstream keeps your fork up to date.}
+  spec.description   = %q{Automatically fetch from upstream every hour.}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "whenever"
 end
